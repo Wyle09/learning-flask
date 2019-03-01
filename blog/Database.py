97be5773a3b69d89dfe1b9f19404675278884468
Wyle09 <wylecordero@gmail.com>
2019-03-01 14:07:42 -0500

    First commit, flask blog app

---
+++ blog/Database.py
@@ -0,0 +1,23 @@
+import pyodbc
+import yaml
+
+
+class Database:
+    def __init__(self, query):
+        self.query = query
+
+    def sql_server_connection(self):
+        db_config = yaml.load(open('config/db.yaml'))
+        conn = pyodbc.connect(driver=db_config['driver'],
+                              host=db_config['server'],
+                              database=db_config['db'],
+                              user_id=db_config['uid'],
+                              password=db_config['pwd'])
+        return conn
+
+    def execute_query(self):
+        conn = self.sql_server_connection()
+        cur = conn.cursor()
+        cur.execute(self.query)
+        conn.commit()
+        return conn, cur
