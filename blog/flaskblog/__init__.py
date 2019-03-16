6817550879471e056aeec8c6116c0df03188a3fd
Wyle09 <wylecordero@gmail.com>
2019-03-15 23:57:26 -0400

    Fininshing user authentication

--- blog/flaskblog/__init__.py
+++ blog/flaskblog/__init__.py
@@ -11,3 +11,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
--- blog/flaskblog/users/routes.py
+++ blog/flaskblog/users/routes.py
@@ -1,10 +1,10 @@
 """ Module contains user related routes """
-from flask import render_template, url_for, flash, redirect
-from flaskblog.user.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog.users.forms import RegistrationForm, LoginForm
 from flask import Blueprint
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import (login_user, current_user, logout_user, login_required)
 
 
 user = Blueprint('user', __name__)
@@ -12,6 +12,8 @@ user = Blueprint('user', __name__)
 
 @user.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         encrypt_pwd = bcrypt.generate_password_hash(
@@ -27,6 +29,8 @@ def register():
 
 @user.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return render_template(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -34,9 +38,27 @@ def login():
         # verify that the user list is not empty and compare the passwords
         # from the database to the password enter by the user in the
         # login form.
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(
+                user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                redirect(url_for('main.home'))
         else:
-            flash("Login unsucessful. Please check username and password", 'danger')
+            flash("Login unsucessful. Please check username and password",
+                  'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@user.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+
+@user.route('/account')
+@login_required  # User needs to log in before accessing this route.
+def account():
+    return render_template('account.html', title='Account')
