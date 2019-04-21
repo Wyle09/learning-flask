de8b2c2b9446001d6545fc7672cf0b4ca6b077a2
root <root@LAPTOP-F9RBL54D.localdomain>
2019-04-21 19:58:40 -0400

    Updating main.css

--- blog/tests/browserdemo.py
+++ blog/tests/browserdemo.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 
 
-driver = webdriver.Chrome('C:\\my-projects\\python\\learning-flask\\blog\\test\\drivers\\chromedriver') # noqa
+driver = webdriver.Chrome('C:\\my-projects\\python\\learning-flask\\blog\\tests\\drivers\\chromedriver') # noqa
 driver.get('http://localhost:5000/home')
 
 print(driver.title)
